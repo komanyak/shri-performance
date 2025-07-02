8e553d25ecf3b59847679f441133b7474e8d1434
unknown <n.d.2002z@gmail.com>
2025-07-02 14:11:28 +0300

    init

---
+++ src/components/Event.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+
+export default function Event(props) {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current && props.onSize) {
+      const { width, height } = ref.current.getBoundingClientRect();
+      props.onSize({ width, height });
+    }
+  });
+
+  return (
+    <li ref={ref} className={`event ${props.slim ? "event_slim" : ""}`}>
+      <button className="event__button">
+        <span
+          className={`event__icon event__icon_${props.icon}`}
+          role="img"
+          aria-label={props.iconLabel}
+        ></span>
+        <h4 className="event__title">{props.title}</h4>
+        {props.subtitle && (
+          <span className="event__subtitle">{props.subtitle}</span>
+        )}
+      </button>
+    </li>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/styles.css";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
