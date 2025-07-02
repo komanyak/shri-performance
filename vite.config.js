dc7cf1ee37935bafe649b4f01ba032131e97d912
unknown <n.d.2002z@gmail.com>
2025-07-02 15:13:02 +0300

    init vite

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "shri2025-task",
   plugins: [react()],
-//   build: {
-//     assetsInlineLimit: 0,
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           react: ["react", "react-dom"],
-//         },
-//       },
-//     },
-//   },
+  //   build: {
+  //     assetsInlineLimit: 0,
+  //     rollupOptions: {
+  //       output: {
+  //         manualChunks: {
+  //           react: ["react", "react-dom"],
+  //         },
+  //       },
+  //     },
+  //   },
 });
